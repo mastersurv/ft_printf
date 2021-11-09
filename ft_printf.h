43cc17f990d6c0f594e845ced5e3578cc3d709e1
mastersurv <mastersurv.nikiforov@yandex.ru>
2021-11-09 17:52:22 +0300

    .h

--- ft_printf.h
+++ ft_printf.h
@@ -2,7 +2,7 @@
 // Created by Lela Ayano on 11/9/21.
 //
 
-#ifndef FT_PRINTF_FT_PRINTF_H
-#define FT_PRINTF_FT_PRINTF_H
+#ifndef FT_PRINTF_H
+#define FT_PRINTF_H
 
-#endif //FT_PRINTF_FT_PRINTF_H
+#endif //FT_PRINTF_H
